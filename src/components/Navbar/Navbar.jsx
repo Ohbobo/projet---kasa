500bd94c81d7bbd8f29a7a859657a99cd2953a7e
theo <agen.theo0@gmail.com>
2023-07-27 15:19:08 +0200

    underline link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,15 +7,27 @@ import "./Navbar.scss"
 
 const NavBar = () => {
 
+    const [underlineLink, setUnderlineLink] = useState("");
+
+    const handleClick = (link) => {
+        setUnderlineLink(link);
+    }
+
     return (
         <div className="navbar">
             <Link to="/"><img src={logo} className="navbar__logo" alt="Logo kasa" /></Link>
             <div className="navbar-menu">
-                <Link className="navbar-menu__link" to="/">Accueil</Link>
-                <Link className="navbar-menu__link" to="/about">A propos</Link>
+                <Link className={`navbar-menu__link ${underlineLink === "/" ? "active" : ""}`} 
+                to="/"
+                onClick={() => {handleClick("/")}}>Accueil</Link>
+
+                <Link className={`navbar-menu__link ${underlineLink === "/about" ? "active" : ""}`} 
+                to="/about"
+                onClick={() => {handleClick("/about")}}>A propos</Link>
             </div>
         </div>
     )
 }
 
 export default NavBar;
+
