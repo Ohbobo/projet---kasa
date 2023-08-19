2fab829786ca61b1fce117b8d73a66cd29adbf83
BuildTools <unconfigured@null.spigotmc.org>
2023-08-19 09:55:24 +0200

    transition collapse

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,29 +1,27 @@
-import { useState } from "react"
-import chevron from "../../assets/chevron.png"
+import React, { useState } from "react";
+import chevron from "../../assets/chevron.png";
+import "./Collapse.scss";
 
-import "./Collapse.scss"
-
-const Collapse = ({title, text}) => {
-
+const Collapse = ({ title, text }) => {
     const [open, setOpen] = useState(false);
 
     const handleClick = () => {
         setOpen(!open);
-    }
+    };
 
     return (
-        <div className="collapse-container">
-
+        <div className={`collapse-container ${open ? "active" : ""}`}>
             <div className="collapse-content" onClick={handleClick}>
-            <p className="collapse-content__title">{title}</p>
-            <img className={`collapse-content__img ${open ? "active" : ""}`} src={chevron} alt="chevron" />
+                <p className="collapse-content__title">{title}</p>
+                <img className={`collapse-content__img ${open ? "active" : ""}`} src={chevron} alt="chevron" />
             </div>
-
             {open && <p className="collapse-content__text">{text}</p>}
         </div>
-    )
+    );
+};
+
+export default Collapse;
 
 
-}
 
-export default Collapse; 
+
